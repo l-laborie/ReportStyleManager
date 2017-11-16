13fff6e7c0f25efdc8bce7ce3ec785cfb5fce220
Ludovic Laborie <l.laborie@criteo.com>
2017-11-16 15:59:15 -0500

    Add Test Project

---
+++ ReportStyleModifierTest/TestDescriptor.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ReportStyleModifier;
+
+namespace ReportModifierTest
+{
+    [TestClass]
+    public class StyleDescriptor
+    {
+        private string[] pass_inputs = new string[]
+        {
+            "CHART_TITLE_Font_Family",
+            "CHART_SUBTITLE_Font_Color",
+            "MAIN_SUBTITLE_Font_Color",
+            "TITLE_Font_Family",
+            "TABLE_TITLE_Font_Family",
+            "TABLE_SUBTITLE_Font_Color",
+        };
+
+        [TestMethod]
+        public void TestPass()
+        {
+            Descriptor descriptor;
+
+            descriptor = new Descriptor(
+                this.pass_inputs, "_");
+
+            List<String> level1 = new List<String>(
+                descriptor.GetLevel1Values());
+            Assert.AreEqual(4, level1.Count);
+            Assert.IsTrue(level1.Contains("MAIN"));
+            Assert.IsTrue(level1.Contains("_MAIN_"));
+            Assert.IsTrue(level1.Contains("TABLE"));
+            Assert.IsTrue(level1.Contains("CHART"));
+
+            foreach(string key in level1)
+            {
+                List<String> level2 = new List<String>(
+                descriptor.GetLevel2Values(key));
+                if (key.Contains("MAIN"))
+                    Assert.AreEqual(1, level2.Count);
+                else
+                    Assert.AreEqual(2, level2.Count);
+                if (key != "MAIN")
+                    Assert.IsTrue(level2.Contains("TITLE"));
+                if (key != "_MAIN_")
+                    Assert.IsTrue(level2.Contains("SUBTITLE"));
+
+                foreach(string key2 in level2)
+                {
+                    List<string> values = new List<string>(
+                        descriptor.GetLevel3Values(key, key2));
+                    Assert.AreEqual(1, values.Count);
+                    Assert.IsTrue(values.Contains("Font"));
+                    values = new List<string>(
+                        descriptor.GetLevel4Values(key, key2, "Font"));
+                    if (key2 == "TITLE")
+                        Assert.IsTrue(values.Contains("Family"));
+                    else
+                        Assert.IsTrue(values.Contains("Color"));
+                }
+            }
+        }
+    }
+}
---
+++ ReportStyleModifierTest/TestReportModifier.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.XmlDiffPatch;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Text;
+using System.Xml;
+using ReportStyleModifier;
+
+namespace ReportStyleModifierTest
+{
+    [TestClass]
+    public class TestReportModifier
+    {
+        public bool CompareXml(XmlNode originalNode, XmlNode finalNode, XmlWriter diffGramWriter)
+        {
+            XmlDiff xmldiff = new XmlDiff(XmlDiffOptions.IgnoreChildOrder |
+                XmlDiffOptions.IgnoreNamespaces | XmlDiffOptions.IgnorePrefixes);
+
+            bool bIdentical = xmldiff.Compare(originalNode, finalNode, diffGramWriter);
+            diffGramWriter.Close();
+            return bIdentical;
+        }
+
+        [TestMethod]
+        public void TestPass()
+        {
+            XmlDocument original_report = new XmlDocument();
+            original_report.LoadXml(Properties.Resources.report_rdl);
+
+            XmlDocument expected_report = new XmlDocument();
+            expected_report.LoadXml(Properties.Resources.report_expected_rdl);
+
+            ReportModifier report_modifier;
+            report_modifier = new ReportModifier(original_report);
+            report_modifier.SetManagedStyle("dsReportStyle");
+
+            StringBuilder output = new StringBuilder();
+            XmlWriter differences = XmlWriter.Create(output);
+
+            bool are_same = this.CompareXml(
+                original_report.DocumentElement,
+                expected_report.DocumentElement,
+                differences);
+
+            Assert.IsTrue(are_same, output.ToString());
+        }
+    }
+}
